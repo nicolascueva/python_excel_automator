9900dbc1d6139c839c541a2e6ee4b2361950279d
Nicolas Cueva <nicolascueva@outlook.com>
2019-04-04 21:13:32 -0400

    adds bar graph of data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print('hello')
 # Make sure to download openpyxl module
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 wb = xl.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
@@ -12,5 +13,11 @@ for row in range(2, sheet.max_row + 1):
     corrected_price = cell.value * 0.9
     corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
+
+values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=4, max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'E2')
 
 wb.save('transactions2.xlsx')
